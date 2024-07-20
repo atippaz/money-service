8ad747f793eaf0d2cc1cbfbb21fc7a2be18ead5c
atippaz <nasakun13201@gmail.com>
2024-07-20 21:16:44 +0700

    add return when invalid token

--- middlewares/apikey.go
+++ middlewares/apikey.go
@@ -9,7 +9,7 @@ func ApiKeyMiddleware(keySecert string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		apiKeyHeader := c.Get("X-API-KEY")
 		if keySecert != apiKeyHeader {
-			return c.JSON("")
+			return c.Status(fiber.StatusUnauthorized).SendString("Missing token")
 		}
 		return c.Next()
 	}
